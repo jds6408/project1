e7419a7bc8d13de78acbe09963ad0b556328c253
jds6408 <64389087+jds6408@users.noreply.github.com>
2020-05-19 21:22:49 +0900

    Add files via upload

--- dorothy/sub2_artist/common/js/scroll.js
+++ dorothy/sub2_artist/common/js/scroll.js
@@ -11,30 +11,51 @@
          //스크롤top의 좌표를 담는다
          console.log(scroll);
 
-         /*   if (scroll > 350) {
-                $(".content .artist_introduce .profile").css('opacity', 1);
-            } else {
-                $(".content .artist_introduce .profile").css('opacity', 0);
-            }*/
-         if (scroll > 890) {
-             $('.content .artist_introduce .albums ul').css('opacity', 1).css('transition', '1s');
+         if (scroll > 160 && scroll < 800) {
+             $('.content .artist_introduce .profile .intro h3').css('color', '#a8191f');
          } else {
-
-             $('.content .artist_introduce .albums ul').css('opacity', 0).css('transition', '1s');
+             $('.content .artist_introduce .profile .intro h3').css('color', '#b2a8a8');
+         }
+         if (scroll > 800 && scroll < 2000) {
+             $('.content .artist_introduce .albums h3 ').css('color', '#a8191f').css('borderBottom', '2px solid #a8191f');
+         } else {
+             $('.content .artist_introduce .albums h3 ').css('color', '#b2a8a8').css('borderBottom', '2px solid #b2a8a8s');
+         }
+         if (scroll > 2000 && scroll < 4000) {
+             $('.content .video .v_txt ').css('color', '#a8191f').css('borderBottom', '2px solid #a8191f');
+             $('.content .video2 .v_txt ').css('color', '#a8191f').css('borderBottom', '2px solid #a8191f');
+         } else {
+             $('.content .video .v_txt').css('color', '#b2a8a8').css('borderBottom', '2px solid #b2a8a8s');
+             $('.content .video2 .v_txt').css('color', '#b2a8a8').css('borderBottom', '2px solid #b2a8a8s');
          }
 
-         if (scroll > 1800) {
+         if (scroll > 4000) {
+             $('.content .gallery h3 ').css('color', '#a8191f').css('borderBottom', '2px solid #a8191f');
+         } else {
+             $('.content .gallery h3 ').css('color', '#b2a8a8').css('borderBottom', '2px solid #b2a8a8');
+         }
+
+
+
+         if (scroll > 600) {
+             $('.content .artist_introduce .albums ul').css('bottom', 0).css('transition', '1s');
+         } else {
+
+             $('.content .artist_introduce .albums ul').css('bottom', '-1000').css('transition', '1s');
+         }
+
+         if (scroll > 1700) {
              $('.content .video2 .vid').css('opacity', 1).css('transition', '1s');
          } else {
              $('.content .video2 .vid').css('opacity', 0).css('transition', '1s');
          }
-         if (scroll > 2200) {
+         if (scroll > 1700) {
              $('.content .video .vid').css('opacity', 1).css('transition', '1s');
          } else {
              $('.content .video .vid').css('opacity', 0).css('transition', '1s');
          }
 
-         if (scroll > 3200) {
+         if (scroll > 3300) {
              $('.content .gallery div').css('opacity', 1).css('transition', '1s');
          } else {
              $('.content .gallery div').css('opacity', 0).css('transition', '1s');
